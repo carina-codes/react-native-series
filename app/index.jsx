9675f34a83f98ad8631d16fdeda80a043da34b98
Carina Rivera <crivera@nomnomdata.com>
2024-12-09 13:02:34 -0800

    list views

--- app/index.jsx
+++ app/index.jsx
@@ -14,6 +14,14 @@ const App = () => {
       >
         <Text style={styles.title}>Coffee Shop</Text>
 
+        <Link href="/menu" style={{ marginHorizontal: 'auto' }} asChild>
+          <Pressable style={styles.button}>
+            <Text style={styles.buttonText}>
+              View Menu
+            </Text>
+          </Pressable>
+        </Link>
+
         <Link href="/contact" style={{ marginHorizontal: 'auto' }} asChild>
           <Pressable style={styles.button}>
             <Text style={styles.buttonText}>
@@ -47,7 +55,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: 'white',
     backgroundColor: 'rgba(0,0,0,.5)',
-    marginBottom: 120,
+    marginBottom: 100,
   },
   link: {
     fontSize: 42,
@@ -59,10 +67,12 @@ const styles = StyleSheet.create({
   },
   button: {
     height: 60,
+    width: 150,
     borderRadius: 20,
     backgroundColor: 'rgba(0,0,0,.75)',
     padding: 6,
     justifyContent: 'center',
+    marginBottom: 25,
   },
   buttonText: {
     fontSize: 16,
